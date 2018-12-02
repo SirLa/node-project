234ca87cc8d930a4e29ca4a757a9639316d878b5
Sona- <simsonarm@mail.ru>
2018-12-02 17:22:21 +0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -4,3 +4,16 @@ export const modules = {
     User: User,
     Product: Product
 };
+
+// You need to use
+// proposal-export-default-from plugin find here 
+// export-default-from in https://babeljs.io/docs/en/plugins
+
+// using this plugin you cane 
+
+// export { default as User } from "./user";
+// export { default as Product } from "./product";
+
+// and import from index in app.js
+
+// import { User, Product } from "./models/index";
